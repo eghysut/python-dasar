80397dcad03c64c3d2d700a401326d4599c70afc
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-09 09:27:54 +0700

    fix: method index

---
+++ Method-List/method_index.py
@@ -0,0 +1,42 @@
+# -- Method List --
+
+# https://docs.python.org/3/tutorial/datastructures.html?highlight=index#list.index
+
+# fungsi index() mengembalikan posisi pada kemunculan pertama dari nilai yang ditentukan.
+
+# Syntax
+# list.index(elemen)
+
+# Nilai Parameter
+# Parameter             Deskripsi
+# elemen                Dibutuhkan. Jenis apa pun (string, angka, list, dll.). Elemen yang harus dicari
+
+listku = ['alice', 'carl', 'eliot', 'geral']
+i = listku.index('geral')
+print(i)    # 3
+
+# Catatan: fungsi index() ini hanya mengembalikan kemunculan nilai pertama .
+
+listku = [10, 'x', False, 'x', True, 'x']
+print(listku.index('x'))
+# Output:
+# 1
+
+i = listku.index('x')
+print(i)
+# Output:
+# 1
+
+print(listku[i])
+# Output:
+# x
+
+# menggunakan list didalam list atau disebut list bersarang
+listku = [2.5, False, ['python', 'x'], True, 3.5, 'x']
+print(listku.index('x'))
+# Output:
+# 5
+
+print(listku[2].index('x'))
+# Output:
+# 1
