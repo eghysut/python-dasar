ff442980087f8e38c7378387645f101c15ae69b5
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-10 11:48:16 +0700

    fix: method pop

---
+++ Method-List/method_pop.py
@@ -0,0 +1,52 @@
+# -- Method List --
+
+# https://docs.python.org/3/tutorial/datastructures.html?highlight=pop#list.pop
+
+# fungsi pop() menghapus elemen list pada posisi yang ditentukan.
+
+# Syntax
+# list.pop(posisi)
+
+# Nilai Parameter
+# Parameter                 Deskripsi
+# posisi                    Opsional. Angka yang menentukan posisi elemen yang ingin Anda hapus, nilai defaultnya adalah -1, yang mengembalikan item terakhir
+
+listku = ['alice', 'carl', 'eliot', 'geral']
+print(listku)   
+# Output:
+# ['alice', 'carl', 'eliot', 'geral'] 
+
+listku.pop()
+print(listku)   
+# Output:
+# ['alice', 'carl', 'eliot']
+
+listku.pop(1)
+print(listku)   
+# Output:
+# ['alice', 'eliot']
+
+listku.pop(0)
+print(listku)   
+# Output:
+# ['eliot']
+
+listku = ['alice', 'carl', 'eliot', 'geral']
+listku.pop(len(listku) - 2)
+print(listku)   
+# Output:
+# ['alice', 'carl', 'geral']
+
+# Catatan: fungsi pop() mengembalikan nilai yang dihapus.
+
+listku = ['alce', 'carl', 'eliot']
+x = []
+while len(listku):
+    i = listku.pop()
+    print(i)
+    x.append(i)
+print(x)    
+# Output: 
+# ['eliot', 'carl', 'alce']
+
+# jika ingin mempelajari lebih lanjut tentang fungsi-bawaan len() kunjungi folder_name: "Fungsi-Bawaan/fungsi_len.py"
