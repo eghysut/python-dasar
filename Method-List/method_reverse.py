6388227ebd0e878235be60c6ed24f57fa559306a
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-10 11:54:11 +0700

    fix: method reverse

---
+++ Method-List/method_reverse.py
@@ -0,0 +1,31 @@
+# -- Method List --
+
+# https://docs.python.org/3/tutorial/datastructures.html?highlight=reverse#list.reverse
+
+# fungsi reverse() membalikkan urutan pengurutan elemen list.
+
+# Syntax
+# list.reverse()
+
+# Nilai Parameter
+# tidak ada nilai parameter
+
+listku = ['alice', 'carl', 'eliot', 'geral']
+print(listku)   
+# Output:
+# ['alice', 'carl', 'eliot', 'geral']
+
+listku.reverse()
+print(listku)   
+# Output:
+# ['geral', 'eliot', 'carl', 'alice']
+
+listku = ['carl', 'alice', 'eliot', 'bert']
+print(listku)  
+# Output:
+# ['carl', 'alice', 'eliot', 'bert']
+
+listku.reverse()
+print(listku)   
+# Output:
+# ['bert', 'eliot', 'alice', 'carl']
