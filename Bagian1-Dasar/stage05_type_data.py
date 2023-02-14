d0cf78ef0c0ce9ad460075eb1fd0c8d37a6602d1
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-14 14:10:13 +0700

    fix: python type data

--- Bagian1-Dasar/stage05_type_data.py
+++ Bagian1-Dasar/stage05_type_data.py
@@ -16,51 +16,72 @@
 # Menadapatkan tipe data
 # fungsi type() menampilkan tipe data objek apa pun
 x = 5
-print(type(x), x)   # <class 'int'> 5
+print(type(x), x)
+# Output:
+# <class 'int'> 5
 
-
-# Mengatur tipe data
-# di python, tipe data diatur saat anda menetapkan nilai ke variabel
+# Mengatur tipe data di python, 
+# tipe data diatur saat anda menetapkan nilai ke variabel
 
 # tipe set
 str_x = "hello world"
-print(type(str_x), str_x)   # <class 'str'> hello world
+print(type(str_x), str_x)   
+# Output:
+# <class 'str'> hello world
 
 # tipe int
 int_x = 10
-print(type(int_x), int_x)   # <class 'int'> 10
+print(type(int_x), int_x)   
+# Output:
+# <class 'int'> 10
 
 # tipe float
 float_x = 10.5
-print(type(float_x), float_x)   # <class 'float'> 10.5
+print(type(float_x), float_x)   
+# Output:
+# <class 'float'> 10.5
 
 # tipe complex
 complex_x = 1j
-print(type(complex_x), complex_x)   # <class 'complex'> 1j
+print(type(complex_x), complex_x)   
+# Output:
+# <class 'complex'> 1j
 
 # tipe list
 list_x = ['alice', 'carl', 'eliot']
-print(type(list_x), list_x)     # <class 'list'> ['alice', 'carl', 'eliot']
+print(type(list_x), list_x)     
+# Output:
+# <class 'list'> ['alice', 'carl', 'eliot']
 
 # tipe tuple
 tuple_x = ('alice', 'carl', 'eliot')
-print(type(tuple_x), tuple_x)   # <class 'tuple'> ('alice', 'carl', 'eliot')
+print(type(tuple_x), tuple_x)   
+# Output:
+# <class 'tuple'> ('alice', 'carl', 'eliot')
 
 # tipe range
 range_x = range(5)
-print(type(range_x), range_x)   # <class 'range'> range(0,5)
+print(type(range_x), range_x)   
+# Output:
+# <class 'range'> range(0,5)
 
 # tipe dict
 dict_x = {'nama':'alice', 'usia':23}
-print(type(dict_x), dict_x)     # <class 'dict'> {'nama':'alice', 'usia':23}
+print(type(dict_x), dict_x)     
+# Output:
+# <class 'dict'> {'nama':'alice', 'usia':23}
 
 # tipe set
 set_x = {'alice', 'carl', 'eliot'}
-print(type(set_x), set_x)       # <class 'set'> {'alice', 'carl', 'eliot'}
+print(type(set_x), set_x)       
+# Output:
+# <class 'set'> {'alice', 'carl', 'eliot'}
 
 # tipe frozenset
 frozen_x = frozenset({'alice', 'carl', 'eliot'})
-print(type(frozen_x), frozen_x) # <class 'frozenset'> frozenset({'eliot', 'carl', 'alice'})
+print(type(frozen_x), frozen_x) 
+# Output:
+# <class 'frozenset'> frozenset({'eliot', 'carl', 'alice'})
 
 # tipe boolean
 true_x = True
@@ -80,7 +101,9 @@ print(type(bytmemory_x), bytmemory_x)   # <class 'memoryview'> <memory at 0x0000
 
 # tipe tidak ada
 none_x = None
-print(type(none_x), none_x)     # <class 'NoneType'> None
+print(type(none_x), none_x)    
+# Output:
+# <class 'NoneType'> None
 
 #===============================================================================
 # mengatur tipe data tertentu
@@ -89,43 +112,43 @@ print(type(none_x), none_x)     # <class 'NoneType'> None
 
 str_x = str("hello world")
 str_y = str(100)
-print(str_x)
-print(str_y)
+print(str_x)  # hello world
+print(str_y)  # 100
 
-int_x = int("23")
+int_x = int("23") 
 int_y = int(15.500)
-print(int_x)
-print(int_y)
+print(int_x)  # 23
+print(int_y)  # 15.0
 
 float_x = float('15')
 float_y = float(15)
-print(float_x)
-print(float_y)
+print(float_x)  # 15
+print(float_y)  # 15.0
 
 complex_x = complex(1.9999999999j)
-print(complex_x)
+print(complex_x)  # 1.9999999999j
 
 list_x = list(('alice', 'carl', 'eliot'))
 list_y = list(range(5))
-print(list_x)
-print(list_y)
+print(list_x) # ['alice', 'carl', 'eliot']
+print(list_y) # [0, 1, 2, 3, 4]
 
 tuple_x = tuple(['alice', 'carl', 'eliot'])
 tuple_y = tuple('hello world')
-print(tuple_x)
-print(tuple_y, type(tuple_y))
+print(tuple_x)                # ('alice', 'carl', 'eliot')
+print(tuple_y, type(tuple_y)) # ('h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd') <class 'tuple'>
 
 dict_x = dict(nama='alice', usia=23)
-print(dict_x)
+print(dict_x) # {'nama': 'alice', 'usia': 23}
 
 set_str = set('hello world')
 set_list = set(['alice', 'carl', 'eliot'])
 set_tuple = set(('alice', 'carl', 'eliot'))
-print(set_str)
-print(set_list)
-print(set_tuple)
+print(set_str)    # {'o', 'l', 'd', 'r', 'w', ' ', 'h', 'e'}
+print(set_list)   # {'eliot', 'carl', 'alice'}
+print(set_tuple)  # {'eliot', 'carl', 'alice'}
 
 true_x = bool(1)
 false_y = bool(0)
-print(true_x)
-print(false_x)
+print(true_x)     # True
+print(false_x)    # False
