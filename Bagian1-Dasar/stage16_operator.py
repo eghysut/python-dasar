79a0a1c2b42d18feb2b0727c0d314d4c514b3c5f
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-17 13:19:11 +0700

    fix: python operators

---
+++ Bagian1-Dasar/stage16_operator.py
@@ -0,0 +1,28 @@
+# Operator python
+# Operator digunakan untuk melakukan operasi pada variabel dan nilai.
+
+# menggunakan operator + untuk menjumlahkan dua nilai
+print(2 + 3)
+# Output:
+# 5
+
+print(2 + int("5"))
+# Output:
+# 7
+
+# menggunakan operator in untuk mencari nilai tertentu
+x = ['alice', 'carl', 'eliot', 'gerald']
+if 'eliot' in x:
+    print("passed")
+else:
+    print("failed")
+# Output:
+# passed
+
+# menggunakan operator pencocokan
+print(2 == 2) # True
+print(2 != 2) # False
+
+# Catatan: operator & | hanya untuk menangani biner/bytes.
+
+# jika ingin mempelajari lebih lanjut tentang python-operator kunjungi folder_name: "python-operator"
