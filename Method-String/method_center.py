bfa2a8e05ae837cf67fdc688f5574b8c83b2518c
eghysut <49760942+eghysut@users.noreply.github.com>
2023-02-26 14:41:10 +0700

    fix: method center

---
+++ Method-String/method_center.py
@@ -0,0 +1,55 @@
+# -- Method String --
+
+# https://docs.python.org/3/library/stdtypes.html?highlight=center#str.center
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi center() akan berpusat(ketengah) menyelaraskan string, menggunakan karakter tertentu 
+# (defaultnya spasi) sebagai mengisi karakter.
+
+# Syntax
+# string.center(panjang, karakter_opsional)
+
+# Nilai Parameter
+# Parameter             Deskripsi
+# panjang               panjang yang dibutuhkan. panjang string yang di kembalikan
+# karakter              opsional. karakter untuk mengisi spasi yang hilang di setiap sisi. defaultnya adalah (spasi)
+
+# menggunakan parameter panjang
+x = "hello world".center(20)
+print(x)    
+# Output:
+#     hello world    
+
+# menggunakan parameter panjang dan karakter_opsional
+y = "hello world".center(20, '#')
+print(y)    
+# Output:
+# ####hello world#####
+
+print(" HELLO WORLD ".center(20, '.'))  
+# Output:
+# ... HELLO WORLD ....
+
+# Ingat: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+s = "hello world"
+# string melakukan perubahan
+print(s.center(20, '%'))    
+# Output:
+# %%%%hello world%%%%%
+
+# string tidak melakukan perubahan
+print(s)    
+# Output:
+# hello world
+
+# string tidak melakukan perubahan
+s = "hello world"
+s.center(20, '%')
+print(s)    
+# Output:
+# hello world
