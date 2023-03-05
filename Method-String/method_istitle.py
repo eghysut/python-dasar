666199c7a317606338d18e9253e561ae198e4451
eghysut <49760942+eghysut@users.noreply.github.com>
2023-03-05 21:48:58 +0700

    fix: method istitle

---
+++ Method-String/method_istitle.py
@@ -0,0 +1,52 @@
+# -- Method String --
+
+# https://docs.python.org/3/library/stdtypes.html?highlight=istitle#str.istitle
+
+# Catatan: 
+# Semua method/metode/fungsi string mengembalikan nilai baru. 
+# Mereka tidak mengubah string asli.
+
+# fungsi istitle() mengembalikan nilai boolean True jika semua kata dalam teks dimulai dengan huruf besar,
+# DAN sisa kata adalah huruf kecil, jika tidak, maka akan mengembalikan nilai boolean False.
+# Simbol dan angka diabaikan.
+
+# Syntax
+# string.istitle()
+
+# Nilai Parameter
+# tidak ada nilai parameter
+
+x = "Hello World"
+print(x.istitle()) 
+# Output:
+# True
+
+y = "Hello Alice, Carl, Eliot"
+print(y.istitle())  
+# Output:
+# True
+
+print("123 Hello_@World!#1".istitle()) 
+# Output:
+# True
+
+print("#!/Usr/Bin/Python3".istitle()) 
+# Output:
+# True
+
+x = "Hello world"
+print(x.istitle()) 
+# Output:
+# False
+
+y = "Hello Alice, carl, eliot"
+print(y.istitle()) # False
+
+print("#!/Usr/Bin/python3".istitle()) # False
+print("#Hello_world@gmail.com".istitle()) # False
+
+# periksa apakah karakter diawali dengan huruf besar?
+if "Python3.8|Python3.9|Python3.10".istitle():
+    print("passed")
+else:
+    print("failed")
