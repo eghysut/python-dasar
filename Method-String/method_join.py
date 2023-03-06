2247412dcc973f19396465bff2aebc4bbb776426
eghysut <49760942+eghysut@users.noreply.github.com>
2023-03-06 09:29:59 +0700

    fix: method join

--- Method-String/method_join.py
+++ Method-String/method_join.py
@@ -23,42 +23,64 @@
 # menggunakan tipe data string
 s = "hello world"
 x = "#".join(s)
-print(x)    # h#e#l#l#o# #w#o#r#l#d
-print("-".join("12.030")) # 1-2-.-0-3-0
+print(x)    
+# Output:
+# h#e#l#l#o# #w#o#r#l#d
+
+print("-".join("12.030")) 
+# Output:
+# 1-2-.-0-3-0
 
 # menggunakan tipe data list
 l = ['alice', 'carl', 'eliot']
 x = " -> ".join(l)
-print(x)    # alice -> carl -> eliot
+print(x)    
+# Output:
+# alice -> carl -> eliot
 
 list_num = ['alice', 10, 'carl', 20, 'eliot']
 x = " ".join(str(i) for i in list_num)
-print(x)    # alice 10 carl 20 eliot
+print(x)    
+# Output:
+# alice 10 carl 20 eliot
 
 # menggunakan tipe data tuple
 t = ('usr', 'bin', 'python')
 x = "/".join(t)
-print(x)    # usr/bin/python
-print("-".join(str(i) for i in (10, 5.5, 20))) # 10-5.5-20
+print(x)    
+# Output:
+# usr/bin/python
+
+print("-".join(str(i) for i in (10, 5.5, 20))) 
+# Output:
+# 10-5.5-20
 
 # menggunakan tipe data dict
 # Catatan: 
 # Saat menggunakan dict sebagai iterable, nilai yang dikembalikan adalah kuncinya/keys, bukan nilainya/values.
 d = {'nama':'alice', 'usia':'23', 'email':'alice@gmail.com'}
 x = ":".join(d)
-print(x)    # nama:usia:email
-print(":".join(d.values())) # alice:23:alice@gmail.com
+print(x)    
+# Output:
+# nama:usia:email
+
+print(":".join(d.values())) 
+# Output:
+# alice:23:alice@gmail.com
 
 dict_num = {'nama':'alice', 'usia':23, 'nrp':555444}
-print(" ".join(str(val) for val in dict_num.values()))  # alice 23 555444
+print(" ".join(str(val) for val in dict_num.values()))  
+# Output:
+# alice 23 555444
 
-# Berhati-hatilah jika anda memiliki iterabel yang memiliki ukuran/item 1
+# Berhati-hatilah jika anda memiliki iterabel yang memiliki, ukuran atau item 1
 print("b".join('a'))    # a
 print("b".join(['a']))  # a
 print("b".join(('a')))  # a
-# fungsi join() sangat cerdas karena menyisipkan item/data anda di antara string iterable yang ingin anda ikuti,
-# ini berarti bahwa jika anda melewati iterable ukuran/item 1,
+# fungsi join() sangat cerdas karena menyisipkan, item atau data anda di antara string iterable yang ingin anda ikuti,
+# ini berarti bahwa jika anda melewati iterable, ukuran atau item 1,
 # anda tidak akan melihat fungsi join()
+
 print("b".join('ac'))       # abc
 print("b".join(['a', 'c'])) # abc
 print("b".join(('a', 'c'))) # abc
