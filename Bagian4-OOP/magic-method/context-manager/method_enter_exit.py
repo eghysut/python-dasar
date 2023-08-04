10f4b92a180f02b71e8fc6939e650138e1ca9af1
eghysut <49760942+eghysut@users.noreply.github.com>
2023-08-04 19:37:33 +0700

    fix: context managers

--- Bagian4-OOP/magic-method/context-manager/method_enter_exit.py
+++ Bagian4-OOP/magic-method/context-manager/method_enter_exit.py
@@ -1,4 +1,4 @@
-# Magic method __enter__ dan __exit__ adalah dua metode khusus dalam Python 
+# Dalam Python, magic method __enter__ dan __exit__ adalah dua metode khusus dalam Python 
 # yang digunakan dalam context managers exception/pengecuali(exception handling) 
 # dengan menggunakan statement "with". 
 # Kedua metode ini bekerja bersama untuk memberikan perilaku yang diinginkan 
